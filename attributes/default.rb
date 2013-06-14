82bf0c430dd736c8f77a105d6204e4099203ffc0
Trevor Bramwell <bramwelt@osuosl.org>
2013-06-14 10:32:34 -0700

    Cookbook: Racktables - Updates
    Downloads tarball and extracts.
    Setup apache vhost.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default["racktables"]["source"] = "http://sourceforge.net/projects/racktables/files/RackTables-0.20.4.tar.gz/download"
 default["racktables"]["checksum"] = "173a164f4"
 default["racktables"]["dir"] = "/var/www/racktables"
-default["racktables"]["wwwroot"] = "/var/www/racktables/wwwroot"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_attribute "racktables"
-
 include_recipe "apache2"
 
 
@@ -15,38 +13,56 @@ app_name = 'racktables'
 app_config = node[app_name]
 
 src_filename = app_config['source']
-src_filepath = "/tmp/racktables"
+src_filepath = "/tmp/Racktables.tar.gz"
+tmp_path = "/tmp/RackTables-0.20.4"
 extract_path = app_config['dir']
 
+# Packages
+%w{php php-mysql php-pdo php-gd php-snmp php-mbstring
+   php-bcmath}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
 
+# User
+user app_name do
+  comment "racktables user"
+  system true
+  shell "/bin/false"
+end
+
+# WWW Dir
 directory extract_path do
-  owner app_name
-  group app_name
+  owner node['apache']['user']
+  group node['apache']['group']
+  recursive true
   mode 0644
   action :create
 end
 
-remote_file src_filename do
-  source src_filepath
-  checksum src_filepath
+# vhost
+web_app "racktables" do
+  server_name "inventory2"
+  server_aliases ["inventory2.osuosl.org"]
+  docroot app_config['dir']
+end
+
+
+# wget & extract file
+remote_file src_filepath do
+  source src_filename
+  checksum app_config['checksum']
   owner app_name
   group app_name
   action :create_if_missing
 end
 
-#bash 'extract_module' do
-#  cwd ::File.dirname(src_filepath)
-#  code <<-EOH
-#    mkdir -p #{extract_path}
-#    tar xzf #{src_filename} -C #{extract_path}
-#    mv #{extract_path}/*/* #{extract_path}/
-#    EOH
-#  not_if { ::File.exists?(extract_path) }
-#end
-
-%w{httpd php php-mysql php-pdo php-gd php-snmp php-mbstring
-   php-bcmath}.each do |pkg|
-  package pkg do
-    action :install
-  end
+bash 'extract_module' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    tar xzf #{src_filepath}
+    sudo cp -r #{tmp_path}/wwwroot #{extract_path}
+    EOH
+  #not_if { ::File.exists?(extract_path) }
 end
