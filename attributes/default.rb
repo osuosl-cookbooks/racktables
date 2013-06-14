5069ce8262b022b32bf3d61dbbcdd1aa2eea9f4d
Trevor Bramwell <bramwelt@osuosl.org>
2013-06-13 17:02:48 -0700

    Cookbook: Racktables - Pull down latest install
    Just grab the tarball for now.

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default["racktables"]["source"] = "http://sourceforge.net/projects/racktables/files/RackTables-0.20.4.tar.gz/download"
+default["racktables"]["checksum"] = "173a164f4"
+default["racktables"]["dir"] = "/var/www/racktables"
+default["racktables"]["wwwroot"] = "/var/www/racktables/wwwroot"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,47 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_attribute "racktables"
+
+include_recipe "apache2"
+
+
+app_name = 'racktables'
+app_config = node[app_name]
+
+src_filename = app_config['source']
+src_filepath = "/tmp/racktables"
+extract_path = app_config['dir']
+
+
+directory extract_path do
+  owner app_name
+  group app_name
+  mode 0644
+  action :create
+end
+
+remote_file src_filename do
+  source src_filepath
+  checksum src_filepath
+  owner app_name
+  group app_name
+  action :create_if_missing
+end
+
+#bash 'extract_module' do
+#  cwd ::File.dirname(src_filepath)
+#  code <<-EOH
+#    mkdir -p #{extract_path}
+#    tar xzf #{src_filename} -C #{extract_path}
+#    mv #{extract_path}/*/* #{extract_path}/
+#    EOH
+#  not_if { ::File.exists?(extract_path) }
+#end
+
+%w{httpd php php-mysql php-pdo php-gd php-snmp php-mbstring
+   php-bcmath}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
