36592f5a96265a5a9f54ae9389cf0bdbb8b918a5
Rudy Grigar <rudy@grigar.net>
2013-07-22 17:11:25 -0700

    fix syntax

--- recipes/default.rb
+++ recipes/default.rb
@@ -50,7 +50,7 @@ web_app "racktables" do
   server_name "inventory2.osuosl.org"
   server_aliases ["inventory2"]
   docroot app_config['dir']
-  variables (
+  variables(
     :redirect_http    => node['racktables']['redirect_http'],
     :ssl_enabled      => node['racktables']['ssl_enabled'],
     :ssl_listen_ports => node['racktables']['ssl_listen_ports']
