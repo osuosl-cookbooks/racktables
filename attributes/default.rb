c1d8ae022d09de6d6979f3b21e58a14a71a4b1bf
Daniel Takamori <pono@osuosl.org>
2013-08-09 14:45:19 -0700

    racktables fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default["racktables"]["source"] = "http://sourceforge.net/projects/racktables/files/latest/download"
 default["racktables"]["extension"] = "http://www.mogilowski.net/wp-content/plugins/download-monitor/download.php?id=18"
 default["racktables"]["dir"] = "/var/www/"
+default["racktables"]["webroot"] = "/var/www/wwwroot"
 default["racktables"]["redirect_http"] = true
 default["racktables"]["ssl_enabled"] = true
 default["racktables"]["ssl_listen_ports"] = [ 443 ]
