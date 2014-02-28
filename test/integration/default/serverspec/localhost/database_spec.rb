0cc8d36d733ce7667460efef3a41e51edd4d9a5e
Trevor Bramwell <bramwelt@osuosl.org>
2014-02-27 18:25:56 -0800

    Add blank tests for racktables::{database, source}

---
+++ test/integration/default/serverspec/localhost/database_spec.rb
@@ -0,0 +1,8 @@
+# Tests for racktables::database
+require 'serverspec'
+
+describe "Racktables SQL" do
+    # Check the SQL db
+    # Check the user exists
+    # Check for user access
+end
---
+++ test/integration/default/serverspec/localhost/source_spec.rb
@@ -0,0 +1,6 @@
+# Tests for racktables::source
+require 'serverspec'
+
+describe "Racktables Source" do
+    # Extract directory exists and non-empty
+end
