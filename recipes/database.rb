93f2a2ce1115ce2c76bb2c35e1f5b6dd5fa44cfb
Trevor Bramwell <bramwelt@osuosl.org>
2014-03-05 10:36:47 -0800

    Remove the racktables SQL file and resource.

--- recipes/database.rb
+++ recipes/database.rb
@@ -23,7 +23,6 @@ mysql_connection_info = {
     :username => 'root',
     :password => node['mysql']['server_root_password']
 }
-create_tables_sql = "#{Chef::Config['file_cache_path']}/racktables_db.sql"
 create_admin_sql = "#{Chef::Config['file_cache_path']}/racktables_admin.sql"
 
 mysql_database node['racktables']['db']['name'] do
@@ -37,19 +36,6 @@ mysql_database_user node['racktables']['db']['user'] do
     database_name node['racktables']['db']['name']
     privileges [:all]
     action [:create, :grant]
-end
-
-cookbook_file "racktables_db.sql" do
-    path create_tables_sql
-    backup false
-    action :create
-end
-
-mysql_database "create racktables db tables" do
-    connection mysql_connection_info
-    database_name node['racktables']['db']['name']
-    sql { ::File.open(create_tables_sql).read }
-    action :query
 end
 
 template "racktables_admin.sql" do
