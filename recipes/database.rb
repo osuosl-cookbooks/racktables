781056d74d28f5a1b3da8f96371109bee9e13903
Trevor Bramwell <bramwelt@osuosl.org>
2014-03-13 13:35:53 -0700

    Fix "chef_gem'mysql'" issue using database LWRP.

--- recipes/database.rb
+++ recipes/database.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "mysql::server"
-include_recipe "database::mysql"
+include_recipe "mysql::ruby"
+include_recipe "mysql::client"
 
 mysql_connection_info = {
     :host => node['racktables']['db']['host'],
