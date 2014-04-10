d18011fba7631b3da192d76a483deab45a203dd3
Trevor Bramwell <bramwelt@osuosl.org>
2014-04-09 18:30:57 -0700

    Update tests with stubbed db databag.

--- test/unit/spec/application_spec.rb
+++ test/unit/spec/application_spec.rb
@@ -3,6 +3,17 @@ require_relative 'spec_helper'
 describe 'racktables::application' do
     let (:chef_run) { ChefSpec::Runner.new.converge('racktables::application') }
 
+    before do
+        Chef::EncryptedDataBagItem.stub(:load).with('racktables', 'database').and_return(
+            db = {
+                "host" => "mysql.example.com",
+                "name" => "racktables_db",
+                "user" => "racktables_user",
+                "password" => "password"
+            }
+        )
+    end
+
     it 'should create the secret file directory' do
         expect(chef_run).to create_directory('/var/www/html/racktables/current/inc').with(
             recursive: true
--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -6,6 +6,14 @@ describe 'racktables::default' do
     before do
         stub_command("/usr/bin/mysql -u root -e 'show databases;'").and_return("foo")
         stub_command("which php").and_return("/usr/bin/php")
+        Chef::EncryptedDataBagItem.stub(:load).with('racktables', 'database').and_return(
+            db = {
+                "host" => "mysql.example.com",
+                "name" => "racktables_db",
+                "user" => "racktables_user",
+                "password" => "password"
+            }
+        )
     end
 
     it 'should include all recipes' do
